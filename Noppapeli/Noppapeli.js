73b9a0f270513ba60cb9babc5f4bacc171089489
Urho Viinikainen <urkki2@gmail.com>
2019-09-30 13:39:11 +0300

    tein arvot pelaajille ja yritin tehdä nopanheitto laskurin

---
+++ Noppapeli/Noppapeli.js
@@ -0,0 +1,13 @@
+
+var Pelaajat = ["pelaaja0", "pelaaja1", "pelaaja2", "pelaaja3"];
+var score = document.getElementsByClassName("score");
+var nopanHeitto = document.getElementById("noppa"); 
+var numerot = document.getElementById("numerot"); 
+function Pelaajat(elem) { 
+    
+}
+function aloitaPelaaminen() {
+      var x = math.floor((math.random() * 6) + 1);
+        document.getElementById("numerot").innerHTML = x;
+      
+}

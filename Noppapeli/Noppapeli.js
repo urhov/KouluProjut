e693c08866882ea8b1e33cda8365e42b7d0b1dcc
Urho Viinikainen <urkki2@gmail.com>
2019-10-03 13:44:37 +0300

    tein pelaajien näytämistä

--- Noppapeli/Noppapeli.js
+++ Noppapeli/Noppapeli.js
@@ -1,31 +1,45 @@
 let kuvat = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
+let kerroin = [ ,1 ,2 ,3 ,4 ,5 ,6]; 
+// var pelaajat = ["Liisa", "Keke", "Maija", "Matti"];
+// var pisteet = [0,67,0,0];
 
-var Pelaajat = ["pelaaja0", "pelaaja1", "pelaaja2", "pelaaja3"];
+var pelaajat = [
+  { nimi : "Liisa", pisteet : 0 }, 
+  {nimi : "Keke", pisteet : 0 },
+  {nimi : "Sami", pisteet : 0 },
+  {nimi : "Emmi", pisteet : 0 }
+];
+
 var nopanHeitto = document.getElementById("noppa"); 
 var valitseNumerot = document.getElementById("numerot"); 
+var vuoro = 0;
 
-
-function Pelaajat() { 
+function updateUi(){
   
+  // Pelaajien näyttäminen
+  let htmlPelaajat = "";
+  for (let i=0; i<pelaajat.length; i++){
+    htmlPelaajat += pelaajat[i].pisteet + "<br>";
+    htmlPelaajat += pelaajat[i].nimi + "<br>";
+
+  }
+
+  document.getElementById("pelaajat").innerHTML = htmlPelaajat;
 }
+  //näytä pisteet
+      
+    
+  // Näytä pelaajat
+
+
+
 function pelaaNappi() {
     var x = Math.floor((Math.random() * 6) + 1);
     document.getElementById("numerot").innerHTML = x;
 
     document.getElementById("noppaKuva").src = "./Kuva/" + kuvat[x-1];
+
 }
+ 
 
  
-//document.getElementById("s1").src = "s1" = 1; 
-  //  document.getElementById("s2").src = "s2" = 2;
-    //document.getElementById("s3").src = "s3" = 3;  
-    //document.getElementById("s4").src = "s4" = 4; 
-    //document.getElementById("s5").src = "s5" = 5; 
-    //document.getElementById("s6").src = "s6" = 6; 
-
-    //<img src="./Kuva/2.png" id="s2" alt="2 png">
-    //<img src="./Kuva/3.png" id="s3" alt="3 png">
-    //<img src="./Kuva/4.png" id="s4" alt="4 png">
-    //<img src="./Kuva/5.png" id="s5" alt="5 png">
-    //<img src="./Kuva/6.png" id="s6" alt="6 png">
-    

e92c0bdfeead86830a71ffb3969e1be44367c0fa
Urho Viinikainen <urkki2@gmail.com>
2019-10-10 13:21:34 +0300

    tein toisen nopan ja muuta

--- Noppapeli/Noppapeli.js
+++ Noppapeli/Noppapeli.js
@@ -1,7 +1,9 @@
+let x = 1; 
+let f = 1; //nopat
 let kuvat = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
-let kerroin = [ ,1 ,2 ,3 ,4 ,5 ,6]; 
+let kerroin = [ ,1 ,2 ,3 ,4 ,5 ,6, 7, 8, 9, 10, 11, 12]; 
 let pisteet = 0;
-
+// var vuoronVaihto = false; 
 // var pelaajat = ["Liisa", "Keke", "Maija", "Matti"];
 // var pisteet = [0,67,0,0];
 
@@ -30,6 +32,12 @@ function updateUi(){
 
   document.getElementById("vuorossa").innerHTML = pelaajat[vuoro].nimi; 
    
+  document.getElementById("noppaKuva").src = "./Kuva/" + kuvat[x-1];
+  document.getElementById("toinenNoppa").src = "./Kuva/" + kuvat[f-1];
+
+  document.getElementById("htmlPisteet").innerHTML = pisteet;
+  document.getElementById("numerot").innerHTML = f + x;
+
 }
   //näytä pisteet
       
@@ -39,23 +47,29 @@ function updateUi(){
 
 
 function pelaaNappi() {
-  pelaaNappi = true;
-    var x = Math.floor((Math.random() * 6) + 1);
-    document.getElementById("numerot").innerHTML = x;
-    document.getElementById("htmlPisteet").innerHTML = pisteet;
-    document.getElementById("noppaKuva").src = "./Kuva/" + kuvat[x-1];
-    pisteet = pisteet + x;
+    
+    x = Math.floor((Math.random() * 6) + 1);
+    f = Math.floor((Math.random() * 6) + 1);
+   
+    
+    pisteet = pisteet + x + f;
+    
+    if ("numerot"=1){
+
+    }
+    
+    updateUi();
+    
+    
 }
+
 function vuoronVaihto(){
-  
   pelaajat[vuoro].pisteet = pisteet;
-  
   vuoro++;
   if (vuoro >= pelaajat.length){
     vuoro = 0;
     pisteet = 0;
-  }
-    
+  } 
   updateUi();
 }
 

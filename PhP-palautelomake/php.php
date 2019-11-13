4bb48901fe04fe6134d814772750073c0d503d95
Urho Viinikainen <urkki2@gmail.com>
2019-11-13 13:03:27 +0200

    tein xml tiedoston ja xdbug

--- PhP-palautelomake/php.php
+++
@@ -1,3 +0,0 @@
-<?php include 'inc/php.php'?>
-    <h1>koti</h1>
-<?php include 'inc/php.php'?>
---
+++ PhP-palautelomake/save_register.php
@@ -0,0 +1,14 @@
+<?php 
+    $t = $_POST['nimimerkki']; 
+    if (strlen($t)= 10) {
+        echo "nimi on liian pitkä";
+    } 
+?>
+
+
+
+<?php 
+function tiedonTallennus() {
+    
+}
+?>

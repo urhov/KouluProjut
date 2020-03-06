c4272d84d6fc14622f7dea7f227561c6c42898e4
Urho Viinikainen <urkki2@gmail.com>
2020-03-06 09:25:59 +0200

    uusi dumppi

--- Selainkäyttöinen viestintä/database.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE `kommentit` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kommentti` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;m
-

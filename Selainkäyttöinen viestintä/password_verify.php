f6f3830039eb4c82e9cfd11f834ae2d5260547ae
Urho Viinikainen <urho.viinikainen@itvelhot.local>
2020-08-24 10:47:34 +0300

    Poistettiin viestiseinä

--- Selainkäyttöinen viestintä/password_verify.php
+++
@@ -1,9 +0,0 @@
-<?php
-$salasana = "qweerty";
-$hash = '$2y$10$sFm47bNxSYdmaKUbteLZzOMHdupw3CbweufOv8zkisKvEAtogXoye';
-
-if (password_verify($salasana,$hash)){
-    echo "salasana on oikien";
-} else {
-    echo "salasana on väärin";
-}
